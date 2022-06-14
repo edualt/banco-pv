2691eaf86e7ee5fb7196e1e542697480703be857
edualt <eduardosebastianhdz503@gmail.com>
2022-06-13 22:47:56 -0500

    Update

--- js/register.js
+++ js/register.js
@@ -6,10 +6,13 @@ form.addEventListener('submit', (e) => {
     const rfc = document.getElementById('rfc').value;
     const first_name = document.getElementById('first_name').value;
     const last_name = document.getElementById('last_name').value;
+    const cuenta_select = document.getElementById('cuenta_select');
+    const nip = document.getElementById('nip').value;
+    const tipo_cuenta = cuenta_select.options[cuenta_select.selectedIndex].value;
 
-    console.log(rfc, first_name, last_name);
+    console.log(rfc, first_name, last_name, nip, tipo_cuenta);
 
-    if (rfc.length != 0 || first_name.length != 0 || last_name.length != 0) {
+    if (rfc.length != 0 || first_name.length != 0 || last_name.length != 0 || tipo_cuenta == '') {
         fetch('http://127.0.0.1:4000/api/users', {
             method: 'POST',
             headers: {
@@ -18,7 +21,9 @@ form.addEventListener('submit', (e) => {
             body: JSON.stringify({
                 rfc: rfc,
                 first_name: first_name,
-                last_name: last_name
+                last_name: last_name,
+                nip: nip,
+                tipo_cuenta: tipo_cuenta
             })
 
         })
